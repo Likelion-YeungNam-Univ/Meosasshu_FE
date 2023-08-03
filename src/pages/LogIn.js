70c249b4f644522fb4b91798542d84d020203b8c
yn_nub <129060841+soooheeee@users.noreply.github.com>
2023-08-03 19:39:04 +0900

    Update LogIn.js

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,8 +1,44 @@
 import React from "react";
 const LogIn = () => {
     return(
-        <>
-        </>
+        <setion>
+        <div>
+            <form>
+                <div>
+                    <div>
+                        <input
+                         type='id'
+                         id="id"
+                         placeholder="아이디"
+                        />
+                    </div>
+                    <div>
+                    <input
+                        type='password'
+                        id="password"
+                        placeholder="비밀번호"
+                    />
+                    </div>
+                </div>
+
+                <div>
+                    <button>
+                        로그인
+                    </button>
+                </div>
+
+                <p>
+                    <a
+                     href='/SignUp'>
+                     회원가입
+                     </a>
+                </p>
+                  
+            </form>
+        </div>
+
+    </setion>
+    
     )
 }
 export default LogIn
