09c783c6922c55b62bf0afa49fcca9bb7511ed73
mymo <106878420+mymooss@users.noreply.github.com>
2023-08-03 18:41:01 +0900

    [UPDATE] STT 기능의 검색창 완성

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,50 +1,56 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
 import MicIcon from '@mui/icons-material/Mic';
+import styled from 'styled-components';
 
 const Search = () => {
-const [micActive, setMicActive] = useState(false);
+  const [micActive, setMicActive] = useState(false);
   const {
     transcript,
-    listening,
     resetTranscript,
-    browserSupportsSpeechRecognition
+    browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
   if (!browserSupportsSpeechRecognition) {
+    //브라우저가 마이크를 허용하지 않은 경우
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
+  const handleSearch = () => {
+    console.log('검색 아이콘이 클릭되었습니다!');
+  };
+
   const handleMicClick = () => {
     if (!micActive) {
-        console.log('마이크 아이콘이 클릭되었습니다! 음성 인식 시작.');
-        SpeechRecognition.startListening({ continuous: true, language: 'ko' });
-      } else {
-        console.log('마이크 아이콘이 클릭되었습니다! 음성 인식 중지.');
-        SpeechRecognition.stopListening();
-      }
-      setMicActive(!micActive);
+      console.log('마이크 아이콘이 클릭되었습니다! 음성 인식 시작.');
+      SpeechRecognition.startListening({ continuous: true, language: 'ko' });
+    } else {
+      console.log('마이크 아이콘이 클릭되었습니다! 음성 인식 중지.');
+      SpeechRecognition.stopListening();
+      console.log(transcript)
+      resetTranscript(); 
+    }
+    setMicActive(!micActive);
   };
+
   return (
     <div>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
-      <TextField
-        placeholder="검색어를 입력하세요"
+      <WideTextField
+        placeholder=" 찾으시는 물건이 무엇인가요?"
         variant="outlined"
         InputProps={{
-          startAdornment: <SearchIcon />,
-          endAdornment: (
-            <MicIcon onClick={handleMicClick} style={{ cursor: 'pointer' }} />
-          ),
+          startAdornment: <SearchIcon onClick={handleSearch} style={{ cursor: 'pointer' }} />,
+          endAdornment: <MicIcon onClick={handleMicClick} style={{ cursor: 'pointer' }} />,
         }}
       />
     </div>
   );
 };
+
+const WideTextField = styled(TextField)`
+  width: 400px; /* Set the desired width here */
+`;
 
 export default Search;
