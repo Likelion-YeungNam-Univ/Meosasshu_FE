5ff008a2ff99dc338584252fcabf2a2b85856e1a
yn_nub <129060841+soooheeee@users.noreply.github.com>
2023-08-04 17:05:53 +0900

    [FEAT]이메일 작성 확인 기능

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,44 +1,71 @@
-import React from "react";
-const LogIn = () => {
-    return(
-        <setion>
-        <div>
-            <form>
+
+import React, { useState } from 'react';
+
+
+const Login = () => {
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  
+  const [idValid, setIdValid] = useState(false);
+
+  const handleId = (e) => {
+    setId(e.target.value);
+    const regex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    if (regex.test(e.target.value)) {
+      setIdValid(true);
+    } else {
+      setIdValid(false);
+    }
+  };
+
+
+  return(
+    <setion>
+    <div>
+        <form>
+            <div>
                 <div>
-                    <div>
-                        <input
-                         type='id'
-                         id="id"
-                         placeholder="아이디"
-                        />
-                    </div>
-                    <div>
                     <input
-                        type='password'
-                        id="password"
-                        placeholder="비밀번호"
+                     type='id'
+                     id="id"
+                     placeholder="아이디"
+                     value={id}
+                     onChange={handleId} 
+                     required //아무것도 입력되지 않았을 때, 입력하라는 문구
                     />
-                    </div>
                 </div>
-
                 <div>
-                    <button>
-                        로그인
-                    </button>
+                    {!idValid && id.length > 0 && (<div>올바른 이메일을 입력해주세요.</div>)}
                 </div>
+                <div>
+                 <input
+                    type='password'
+                    id="password"
+                    placeholder="비밀번호"
+                    value={pw}
+                    onChange={(e) => setPw(e.target.value)}
+                    required //아무것도 입력되지 않았을 때, 입력하라는 문구
+                 />
+                </div>
+            </div>
 
-                <p>
-                    <a
-                     href='/SignUp'>
-                     회원가입
-                     </a>
-                </p>
-                  
-            </form>
-        </div>
-
-    </setion>
-    
-    )
+            <div>
+                <button>
+                    로그인
+                </button>
+            </div>
+            <p>
+                <a
+                 href='/SignUp'>
+                 회원가입
+                 </a>
+            </p>
+        </form>
+    </div>
+</setion>
+)
 }
-export default LogIn
+
+
+export default Login;
