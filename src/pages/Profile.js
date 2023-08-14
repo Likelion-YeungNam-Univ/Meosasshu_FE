c594f122657faf827efae2656a5c35318a9a6f6b
mymo <106878420+mymooss@users.noreply.github.com>
2023-08-14 14:31:18 +0900

    [FIX] profile.js 충돌 에러

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,18 +1,10 @@
 import React from "react";
-import Header from "../components/Header";
-<<<<<<< HEAD
 import Nav from "../components/Nav";
-=======
->>>>>>> cdfac30c681e6e51981c8f41030f26021d187025
 const Profile = () => {
     const title = "내 정보";
     return(
         <>
-<<<<<<< HEAD
-        <Nav>내 정보</Nav>
-=======
-        <Header children = {title}/>
->>>>>>> cdfac30c681e6e51981c8f41030f26021d187025
+        <Nav children = {title}/>
         </>
     )
 }
