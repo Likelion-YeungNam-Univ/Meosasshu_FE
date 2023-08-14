6b5294908bafecc19b1ef6f5c29db4885f210782
JJyen <gdb1632@naver.com>
2023-08-14 21:25:55 +0900

    [UPDATE] api 연결

--- src/components/Postcode.js
+++ src/components/Postcode.js
@@ -3,12 +3,57 @@ import { useState } from 'react';
 import { useDaumPostcodePopup } from 'react-daum-postcode';
 import styled from 'styled-components'
 
+const Postcode = () => {
+  const open = useDaumPostcodePopup("//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js");
+
+  const [zonecode,setZonecode]=useState("")
+  const [address,setAddress]=useState("")
+
+  const handleComplete = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = '';
+
+    if (data.addressType === 'R') {
+      if (data.bname !== '') {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== '') {
+        extraAddress += extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
+    }
+
+    setZonecode(data.zonecode);
+    setAddress(fullAddress);
+
+    //console.log(data); 
+
+  };
+
+  const handleClick = () => {
+    open({ onComplete: handleComplete });
+  };
+
+  return (
+        <div>
+            <FormListAddress>배송지 등록</FormListAddress>
+            <PostInputBox type="text" placeholder="받는 분"></PostInputBox><br/>
+            <InputZonecode placeholder="우편번호" defaultValue={zonecode}></InputZonecode>
+            <SearchAddBtn type='button' onClick={handleClick}>주소찾기</SearchAddBtn><br/>
+            <PostInputBox placeholder="주소" defaultValue={address}></PostInputBox><br/>
+            <PostInputBox type="text" placeholder="상세주소"></PostInputBox>
+        </div>
+  );
+};
+
+export default Postcode;
+
 const FormListAddress = styled.div`
 font-size: 2vh;
 font-weight: bold;
 `;
 
-const InputBox = styled.input`
+const PostInputBox = styled.input`
 width: 350px ;
 height: 40px;
 border: 2px solid #DEDEDE;
@@ -47,50 +92,3 @@ font-size: 1.7vh;
     padding:0vh;
     font-size: 1.5vh;
 }`;
-
-const Postcode = () => {
-  const open = useDaumPostcodePopup("//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js");
-
-  const [zonecode,setZonecode]=useState("")
-  const [address,setAddress]=useState("")
-
-  const handleComplete = (data) => {
-    let fullAddress = data.address;
-    let extraAddress = '';
-
-    if (data.addressType === 'R') {
-      if (data.bname !== '') {
-        extraAddress += data.bname;
-      }
-      if (data.buildingName !== '') {
-        extraAddress += extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
-      }
-      fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
-    }
-
-    setZonecode(data.zonecode);
-    setAddress(fullAddress);
-
-    console.log(fullAddress); //'서울 성동구 왕십리로2길 20 (성수동1가)'
-    console.log(data.zonecode); //우편번호
-    console.log(data); 
-
-  };
-
-  const handleClick = () => {
-    open({ onComplete: handleComplete });
-  };
-
-  return (
-        <div>
-            <FormListAddress>배송지 등록</FormListAddress>
-            <InputBox type="text" placeholder="받는 분"></InputBox><br/>
-            <InputZonecode placeholder="우편번호" defaultValue={zonecode}></InputZonecode>
-            <SearchAddBtn type='button' onClick={handleClick}>주소찾기</SearchAddBtn><br/>
-            <InputBox placeholder="주소" defaultValue={address}></InputBox><br/>
-            <InputBox type="text" placeholder="상세주소"></InputBox>
-        </div>
-  );
-};
-
-export default Postcode;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,127 +1,39 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styled from 'styled-components'
-import Postcode from "../components/Postcode";
 import logoPath from '../assets/logo2.png';
-
-const FormList = styled.div`
-font-size: 2vh;
-font-weight: bold;
-`;
-const SignUpForm = styled.div`
-//background-color: lightblue;
-width:365px;
-display: flex;
-align-items: center;
-margin: auto;
-
-@media only screen and (max-width: 430px) {
-    //background-color: pink;
-    width:84vw;
-}`;
-const InputBox = styled.input`
-width: 350px ;
-height: 40px;
-border: 2px solid #DEDEDE;
-margin-top: 3px;
-margin-bottom: 8px;
-
-@media only screen and (max-width: 430px) {
-    width: 81vw;
-    height: 5vh;
-}`;
-// const InputTel = styled.input`
-// width: 277px ;
-// height: 40px;
-// border: 2px solid #DEDEDE;
-// margin-top: 3px;
-// margin-botton: 4px;
-// margin-right: 4px;
-
-// @media only screen and (max-width: 430px) {
-//     width: 59.5vw;
-//     height: 5vh;
-// }`;
-const InputYearDay = styled.input`
-width: 112px ;
-height: 40px;
-border: 2px solid #DEDEDE;
-margin-right: 4px;
-margin-top: 3px;
-margin-bottom: 8px;
-
-&::-webkit-inner-spin-button{
-    -webkit-appearance: none; 
-    margin: 0; 
-    }
-&::-webkit-outer-spin-button{
-    -webkit-appearance: none; 
-    margin: 0; 
-    }
-
-@media only screen and (max-width: 430px) {
-    width: 25vw;
-    height: 5vh;
-}`;
-const MonthOption = styled.select`
-width: 112px ;
-height: 44px;
-border: 2px solid #DEDEDE;
-margin-right: 4px;
-margin-top: 3px;
-margin-bottom: 8px;
-
-@media only screen and (max-width: 430px) {
-    width: 26vw;
-    height: 5.5vh;
-}`;
-const GenderOption = styled.select`
-width: 360px ;
-height: 45px;
-border: 2px solid #DEDEDE;
-margin-top: 3px;
-margin-bottom: 8px;
-
-@media only screen and (max-width: 430px) {
-    width: 82vw;
-    height: 5.5vh;
-}`;
-
-const SignUpBtn= styled.input`
-width: 355px ;
-height: 45px;
-background-color: #BCC454;
-border: none;
-color: white;
-font-weight: bold;
-font-size: 15px;
-margin-bottom: 20px;
-
-@media only screen and (max-width: 430px) {
-    width: 83vw;
-    height: 6vh;
-}`;
-
-const Logo= styled.img`
-display: block;
-margin: auto;
-margin-top:20px;
-
-@media only screen and (max-width: 430px) {
-    width: 83vw;
-    height: 8vh;
-    margin-bottom:10px;
-}
-`;
-
-const ConfirmPwMsg= styled.p`
-color: #EB0000;
-margin-top: 0px;
-margin-bottom: 10px;
-font-size: 2.3vh`
-
+import { useDaumPostcodePopup } from 'react-daum-postcode';
 
 const SignUp = () => {
+    const open = useDaumPostcodePopup("//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js");
 
+    const [zonecode,setZonecode]=useState("")
+    const [address,setAddress]=useState("")
+  
+    const handleComplete = (data) => {
+      let fullAddress = data.address;
+      let extraAddress = '';
+  
+      if (data.addressType === 'R') {
+        if (data.bname !== '') {
+          extraAddress += data.bname;
+        }
+        if (data.buildingName !== '') {
+          extraAddress += extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
+        }
+        fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
+      }
+  
+      setZonecode(data.zonecode);
+      setAddress(fullAddress);
+   
+      //console.log(data); 
+  
+    };
+    const handleClick = () => {
+      open({ onComplete: handleComplete });
+    };
+    
     const [email,setEmail]=useState("");
     const [pw,setPw]=useState("");
     const [confirmPw,setConfirmPw]=useState("");
@@ -133,31 +45,27 @@ const SignUp = () => {
     const [gender, setGender]=useState("")
     const [tel,setTel]=useState("")
 
-
     const monthOptions = [
-        {key:1, value:"1월"},
-        {key:2, value:"2월"},
-        {key:3, value:"3월"},
-        {key:4, value:"4월"},
-        {key:5, value:"5월"},
-        {key:6, value:"6월"},
-        {key:7, value:"7월"},
-        {key:8, value:"8월"},
-        {key:9, value:"9월"},
-        {key:10, value:"10월"},
-        {key:11, value:"11월"},
-        {key:12, value:"12월"},
+        {key:1, value: "01", type:"1월"},
+        {key:2, value: "02", type:"2월"},
+        {key:3, value: "03", type:"3월"},
+        {key:4, value: "04", type:"4월"},
+        {key:5, value: "05", type:"5월"},
+        {key:6, value: "06", type:"6월"},
+        {key:7, value: "07", type:"7월"},
+        {key:8, value: "08", type:"8월"},
+        {key:9, value: "09", type:"9월"},
+        {key:10, value: "10", type:"10월"},
+        {key:11, value: "11", type:"11월"},
+        {key:12, value: "12", type:"12월"},
     ]
 
     const genderOption = [
-        {key:1, value:"남자"},
-        {key:2, value:"여자"},
+        {key:2, value:"f", type: "남자"},
+        {key:1, value:"m", type: "여자"},
     ]
 
-
     useEffect(() => {
-          console.log("password : ", pw);
-          console.log("confirmPasswordMsg : ", confirmPwMsg)
         if(pw !== confirmPw){
             setConfirmPwMsg('비밀번호가 일치하지 않습니다.')
         }
@@ -165,8 +73,34 @@ const SignUp = () => {
             setConfirmPwMsg('')
         }
           // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [pw, confirmPw]      
+      }, [pw, confirmPw]   
 )
+
+
+const birthday = `${year}-${month}-${day}`;
+
+
+const apiUrl = 'https://1d75-222-233-66-35.ngrok-free.app';
+
+const onSubmit = () => {
+    axios
+    .post(apiUrl + '/api/v1/auth/register', {
+        email: email,
+        password: pw,
+        name: name,
+        mobileNumber: tel,
+        birthDate: birthday,
+        gender: gender,
+        city: address,
+        street: ' ',
+        zipcode: zonecode,
+    },{})
+    .then((response) => {
+        console.log(response);
+        alert("회원가입이 완료되었습니다.")
+      })
+};
+
 
     return(
         <>
@@ -216,7 +150,7 @@ const SignUp = () => {
                     </InputYearDay>
                     <MonthOption onChange={(e)=>{setMonth(e.currentTarget.value)}} value={month}>
                         <option>월</option>
-                        {monthOptions.map((item) => (<option key={item.key} value={item.value}>{item.value}</option>))}
+                        {monthOptions.map((item) => (<option key={item.key} value={item.value}>{item.type}</option>))}
                     </MonthOption>
                     <InputYearDay
                         type="number" 
@@ -229,7 +163,7 @@ const SignUp = () => {
                     <FormList>성별</FormList>
                     <GenderOption onChange={(e)=>{setGender(e.currentTarget.value)}} value={gender}>
                         <option>성별</option>
-                        {genderOption.map((item) => (<option key={item.key} value={item.value}>{item.value}</option>))}
+                        {genderOption.map((item) => (<option key={item.key} value={item.value}>{item.type}</option>))}
                     </GenderOption>
                 </div>
                 <div>
@@ -241,11 +175,145 @@ const SignUp = () => {
                         onChange={(e)=>{setTel(e.target.value)}}>
                     </InputBox>
                 </div>
-                <Postcode/><br/>
-                <SignUpBtn type="submit" value={"가입하기"}></SignUpBtn>
+                <div>
+                    <FormList>배송지 등록</FormList>
+                    <InputBox type="text" placeholder="받는 분"></InputBox><br/>
+                    <InputZonecode placeholder="우편번호" defaultValue={zonecode}></InputZonecode>
+                    <SearchAddBtn type='button' onClick={handleClick}>주소찾기</SearchAddBtn><br/>
+                    <InputBox placeholder="주소" defaultValue={address}></InputBox><br/>
+                    <InputBox type="text" placeholder="상세주소"></InputBox>
+                </div>
+                <SignUpBtn type="submit" value={"가입하기"} onClick={onSubmit}></SignUpBtn>
             </form>
         </SignUpForm>
         </>
     )
 }
 export default SignUp;
+
+const FormList = styled.div`
+font-size: 2vh;
+font-weight: bold;
+`;
+const SignUpForm = styled.div`
+width:365px;
+display: flex;
+align-items: center;
+margin: auto;
+
+@media only screen and (max-width: 430px) {
+    width:84vw;
+}
+`;
+const InputBox = styled.input`
+width: 350px ;
+height: 40px;
+border: 2px solid #DEDEDE;
+margin-top: 3px;
+margin-bottom: 8px;
+
+@media only screen and (max-width: 430px) {
+    width: 81vw;
+    height: 5vh;
+}
+`;
+const InputYearDay = styled.input`
+width: 112px ;
+height: 40px;
+border: 2px solid #DEDEDE;
+margin-right: 4px;
+margin-top: 3px;
+margin-bottom: 8px;
+
+&::-webkit-inner-spin-button{
+    -webkit-appearance: none; 
+    margin: 0; 
+    }
+&::-webkit-outer-spin-button{
+    -webkit-appearance: none; 
+    margin: 0; 
+    }
+
+@media only screen and (max-width: 430px) {
+    width: 25vw;
+    height: 5vh;
+}
+`;
+const MonthOption = styled.select`
+width: 112px ;
+height: 44px;
+border: 2px solid #DEDEDE;
+margin-right: 4px;
+margin-top: 3px;
+margin-bottom: 8px;
+
+@media only screen and (max-width: 430px) {
+    width: 26vw;
+    height: 5.5vh;
+}`;
+const GenderOption = styled.select`
+width: 360px ;
+height: 45px;
+border: 2px solid #DEDEDE;
+margin-top: 3px;
+margin-bottom: 8px;
+
+@media only screen and (max-width: 430px) {
+    width: 82vw;
+    height: 5.5vh;
+}
+`;
+const SignUpBtn= styled.input`
+width: 355px ;
+height: 45px;
+background-color: #BCC454;
+border: none;
+color: white;
+font-weight: bold;
+font-size: 15px;
+margin-bottom: 20px;
+
+@media only screen and (max-width: 430px) {
+    width: 83vw;
+    height: 6vh;
+}`;
+const Logo= styled.img`
+display: block;
+margin: auto;
+margin-top:20px;
+`;
+const ConfirmPwMsg= styled.p`
+color: #EB0000;
+margin-top: 0px;
+margin-bottom: 10px;
+font-size: 2.3vh
+`;
+
+
+const InputZonecode = styled.input`
+width: 277px ;
+height: 40px;
+border: 2px solid #DEDEDE;
+margin-top: 3px;
+margin-botton: 4px;
+margin-right: 4px;
+
+@media only screen and (max-width: 430px) {
+    width: 59.5vw;
+    height: 5vh;
+}`;
+const SearchAddBtn= styled.button`
+width: 69px ;
+height: 45px;
+font-size: 10px;
+background-color: #BCC454;
+border: none;
+color: white;
+font-size: 1.7vh;
+
+@media only screen and (max-width: 430px) {
+    width: 20vw;
+    height: 5.5vh;
+    padding:0vh;
+    font-size: 1.5vh;
+}`;
