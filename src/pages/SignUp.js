28524443baa768a56a85dad5ca3e698416f90c74
yn_nub <129060841+soooheeee@users.noreply.github.com>
2023-08-18 18:36:54 +0900

    [DELET] 이메일 형식 입력 팝업창  제거

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -110,7 +110,7 @@ const onSubmit = () => {
                 <div>
                     <FormList>아이디</FormList>
                     <InputBox 
-                        type="email" 
+                        type="text" 
                         value={email}
                         onChange={(e)=>{setEmail(e.target.value)}}>
                     </InputBox>
