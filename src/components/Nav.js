3b93874cae770469c9fbc9d1a67b08541223b4e6
yn_nub <129060841+soooheeee@users.noreply.github.com>
2023-08-18 17:10:06 +0900

    [UI] ui 수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,33 +1,53 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const NavbarContainer = styled.div`
-  display: flex;
-  justify-content: center;  // 항목들을 중앙에 배치
-  align-items: center;
-  margin-top: 20px;
-  margin-bottom: 20px;
-
-  @media only screen and (max-width: 430px) {
-      max-width: auto;
-  }
+const BackButton = styled(Link)`
+margin-left:5px;
+  
+  
 `;
 
-const BackButton = styled(Link)`
-  position: absolute;  // 절대 위치 지정
-  left: 5px;  // 왼쪽으로 부터의 거리
+const NavBox = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+
+@media only screen and (min-width: 430px) {
+    width:365px;
+    margin: auto;
+}
+
+@media only screen and (max-width: 430px) {
+max-width: auto;
+margin: auto;
+}`;
+
+
+const NavContent = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const Nav = ({ backTo = '/', children }) => {
   return (
-    <NavbarContainer>
+    <NavBox>
+      {/* <NavbarContainer> */}
        <BackButton to={backTo}>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
           <path d="M16 7H3.83L9.42 1.41L8 0L0 8L8 16L9.41 14.59L3.83 9H16V7Z" fill="black"/>
         </svg>
       </BackButton>
-      <span style={{ fontSize: '20px', fontWeight: '600' }}>{children}</span>
-    </NavbarContainer>
+      <NavContent>
+      <span style={{ fontSize: '20px', fontWeight: '600'}}>{children}</span>
+      </NavContent>
+      <BackButton >
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+          <path d="M16 7H3.83L9.42 1.41L8 0L0 8L8 16L9.41 14.59L3.83 9H16V7Z" fill="#FFF"/>
+        </svg>
+      </BackButton>
+     {/* </NavbarContainer> */}
+    </NavBox>
   );
 };
 
